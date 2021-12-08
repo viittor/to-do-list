6bc47992a55c5e0322355b1a2d3e10d11cea1e8e
Victor Ferrando <victor.ferrando@hotmail.com>
2021-12-08 17:51:13 -0300

    added page elements and started de js code.

---
+++ script.js
@@ -0,0 +1,11 @@
+function addItem(){
+   var item = document.getElementsByClassName("input-item").value;
+   var valor = item;
+   console.log(valor);
+};
+
+function removeItem(){
+
+};
+
+// addEventListener(onclick, document.getElementsByClassName(button).addItem(item));
