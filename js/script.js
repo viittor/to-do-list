b4258700bdc68b20e1ef356ea72e55bc9479986a
Victor Ferrando <victor.ferrando@hotmail.com>
2022-02-22 17:23:09 -0300

    Added style on add button

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function createId() {
     time.getMilliseconds().toString() +
     time.getSeconds().toString() +
     time.getMinutes().toString() +
-    time.getHours().toString()
+    time.getHours().toString();
   return id;
 }
 
@@ -31,7 +31,7 @@ function addTask() {
       },
     };
     tasks.push(task); // empurra o valor da task para o array tasks
-    
+
     myStorage.setItem("tasks", JSON.stringify(tasks));
 
     updateScreen(); //chama a função qye atualiza a tela.
@@ -49,8 +49,10 @@ function updateScreen() {
       task.id +
       "><input type=checkbox></input><div>" +
       task.data.description +
-      "</div><button onclick=removeTask(this) id-data=" + task.id + ">X</button>";
-      "</li>";
+      "</div><button onclick=removeTask(this) id-data=" +
+      task.id +
+      ">X</button>";
+    ("</li>");
 
     // list +=
     //   "<button onclick=removeTask(this) id-data=" + task.id + ">X</button>";
@@ -73,14 +75,12 @@ function removeTask(element) {
   myStorage.setItem("tasks", JSON.stringify(tasks));
 
   loadTasks();
-  
 }
 
+function loadTasks() {
+  let tasks_str = myStorage.getItem("tasks");
 
-function loadTasks(){
-  let tasks_str = myStorage.getItem("tasks")
-
-  if(tasks_str){
+  if (tasks_str) {
     tasks = JSON.parse(tasks_str);
   }
 
